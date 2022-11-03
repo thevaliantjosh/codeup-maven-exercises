445eb7ba14527e988491c36f7a3d4f31ec4d3405
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-11-03 08:08:05 -0500

    First Commit

---
+++ src/main/java/StringUtilsTest.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class StringUtilsTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner;
+        System.out.println("Enter Something: ");
+        String userInput = scanner.nextLine();
+        System.out.printf("You Entered: %s" StringUtils.con);
+    }
+}
