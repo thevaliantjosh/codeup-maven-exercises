8aea8038a011508be0f90f525a86f535fca06b11
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-11-03 08:27:22 -0500

    Maven Exercise Completed

--- src/main/java/StringUtilsTest.java
+++ src/main/java/StringUtilsTest.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 public class StringUtilsTest {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner;
+        Scanner scanner = new Scanner (System.in);
         System.out.println("Enter Something: ");
         String userInput = scanner.nextLine();
-        System.out.printf("You Entered: %s" StringUtils.con);
+
+        if(!StringUtils.isNumeric(userInput)){
+            System.out.printf("You Entered: \"%s\"%n", userInput);
+            System.out.printf("\"%s\" is not a number%n", userInput);
+        }
+        System.out.printf("Flipped Case: %s%n", StringUtils.swapCase(userInput));
+        System.out.printf("Reversed: %s%n", StringUtils.reverse(userInput));
+
     }
 }
